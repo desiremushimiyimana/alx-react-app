62dbc989aecd202e1932d86a59963a98885ee2d0
desiremushimiyimana <desiremushimiyimana888@gmail.com>
2024-08-24 17:39:17 +0200

    Applied inline CSS styling to React components

---
+++ Footer.jsx
@@ -0,0 +1,3 @@
+<footer style={{ backgroundColor: 'darkgray', color: 'white', textAlign: 'center', padding: '10px' }}>
+    <p>© 2024 My Website. All rights reserved.</p>
+</footer>
---
+++ MainContent.jsx
@@ -0,0 +1,3 @@
+<div style={{ padding: '20px', textAlign: 'justify', color: 'darkslategray' }}>
+    {/* Main content goes here */}
+</div>
